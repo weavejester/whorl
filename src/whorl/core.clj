186e55a4cd928042dbf054e3eba31b55baf411fd
James Reeves <jreeves@weavejester.com>
2013-06-24 16:55:24 +0100

    Added fingerprint function

--- src/whorl/core.clj
+++ src/whorl/core.clj
@@ -1,5 +1,6 @@
 (ns whorl.core
-  (:require [clojure.walk :as walk]))
+  (:require [clojure.walk :as walk]
+            [pandect.core :as digest]))
 
 (defn sorted
   "Convert a data structure into an equivalent with a fixed ordering."
@@ -11,3 +12,9 @@
       (set? x) (apply sorted-set x)
       :else    x))
    data))
+
+(defn fingerprint
+  "Return a unique ID for the data structure. Equivalent data structures will
+  always produce the same ID."
+  [data]
+  (digest/sha1 (pr-str (sorted data))))
--- test/whorl/core_test.clj
+++ test/whorl/core_test.clj
@@ -6,3 +6,11 @@
   (let [x (sorted {:z '(2 1) :b 3 :a #{5 4} :c [7 6]})]
     (is (= (pr-str x)
            "{:a #{4 5}, :b 3, :c [7 6], :z (2 1)}"))))
+
+(deftest test-fingerprint
+  (are [x y] (= (fingerprint x) (fingerprint y))
+    {:b 1 :a 2} {:a 2 :b 1}
+    (array-map :b 1 :a 2) (hash-map :a 2 :b 1)
+    #{3 2 1} #{1 3 2}
+    [1 3 2] [1 3 2]
+    {:x {:b 1 :a 2}} {:x {:a 2 :b 1}}))
