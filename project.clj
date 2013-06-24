a30595a084f0556ab6fd7133fbfeec2bae04a3ad
James Reeves <jreeves@weavejester.com>
2013-06-24 17:42:56 +0100

    Updated URL and description in project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject whorl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library for generating fingerprints of Clojure data"
+  :url "https://github.com/weavejester/whorl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
