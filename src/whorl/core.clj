fffe0ded6e9cbfdef70f4c1ac8f44bbc31c05ba2
Gary Fredericks <fredericksgary@gmail.com>
2013-06-28 15:47:53 -0500

    Generalize to allow different data models, add JSON
    It can be useful to fingerprint based on other kinds of equivalency
    besides clojure-runtime equivalency, for example equivalency in a JSON
    serialization. I refactored a bit to allow a `canonizer` function to
    be supplied, and added a fingerprint-json function.

--- src/whorl/core.clj
+++ src/whorl/core.clj
@@ -13,18 +13,52 @@
       :else    x))
    data))
 
-(defn- fingerprint* [data]
-  (digest/sha1 (pr-str (sorted data))))
+(def canonize-edn #'sorted)
+
+;; TODO: verify that the types are JSONable in some sense?
+(defn canonize-json
+  [data]
+  ;; prewalk is necessary rather than postwalk so that
+  ;; we can detect MapEntry. With postwalk they are vanilla
+  ;; vectors by the time the function sees them.
+  (sorted
+   (walk/prewalk
+    (fn [x]
+      (cond
+       (and (vector? x)
+            (not (instance? clojure.lang.MapEntry x)))
+       (seq x)
+
+       (keyword? x) (-> x (str) (subs 1))
+
+       :else x))
+    data)))
+
+(defn- data-sha1
+  [data]
+  (digest/sha1 (pr-str data)))
 
 (defn- weak-hash-map []
   (java.util.Collections/synchronizedMap (java.util.WeakHashMap.)))
 
-(def fingerprint
+(defn cached-fingerprinter
   "Return a unique ID for the data structure. Equivalent data structures will
   always produce the same ID."
+  [canonizer]
   (let [cache (weak-hash-map)]
     (fn [data]
       (or (.get cache data)
-          (let [print (fingerprint* data)]
+          (let [print (data-sha1 (canonizer data))]
             (.put cache data print)
             print)))))
+
+(def fingerprint
+  "Return a unique ID for the data structure. Equivalent data structures will
+  always produce the same ID."
+  (cached-fingerprinter canonize-edn))
+
+(def fingerprint-json
+  "Return a unique ID for the JSON version of the data structure. Keywords
+   are treated the same as strings, and vectors are treated the same as
+   lists."
+  (cached-fingerprinter canonize-json))
